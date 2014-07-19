92b895ac459e649f9deeeb551648ada6d1da265c
nakkul <nakkul@ubuntu.ubuntu-domain>
2014-07-20 04:13:53 +0530

    db integrated with app

--- app/models.py
+++ app/models.py
@@ -1,21 +1,11 @@
+from app import db
 
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask import Flask, jsonify, request
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
-ROLE_USER = 0
-ROLE_ADMIN = 1
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), index = True, unique = True)
-    category = db.Column(db.String(120), index = True)
-    price = db.Column(db.SmallInteger, default = ROLE_USER)
-    rating = db.Column(db.SmallInteger, default = ROLE_USER)
-    reviews = db.Column(db.SmallInteger, default = ROLE_USER)
-    phone = db.Column(db.String(64), index = True)
-
-    def __repr__(self):
-        return '<User %r>' % (self.name)
-
+class Worker(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    category = db.Column(db.String())
+    price = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+    reviews = db.Column(db.Integer)
+    phone = db.Column(db.String())
--- app/views.py
+++ app/views.py
@@ -1,18 +1,14 @@
-
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request
-from app import app,db
+from app import app, db
 import models
 
 @app.route('/fetch/worker', methods=['GET'])
 def get_workers():
     category_ = request.args.get('category')
-    filtered_list = models.User.query.filter_by(category = 'Electrician').all()
+    filtered_list = models.Worker.query.filter_by(category='Electrician').all()
 #    for item in skilled_workers:
  #       if item['category'] == category:
   #          filtered_list.append(item)
 
     return jsonify({'workers': filtered_list})
-
-if __name__ == '__main__':
-    app.run(debug = True)
